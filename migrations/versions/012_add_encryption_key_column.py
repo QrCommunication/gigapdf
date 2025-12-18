a0dc27c2751abc6ae5793fb4ec471d20dbe59530
Rony Licha <ronylicha@gmail.com>
2025-12-18 15:07:49 +0100

    Fix migration down_revision to match actual revision ID

--- migrations/versions/012_add_encryption_key_column.py
+++ migrations/versions/012_add_encryption_key_column.py
@@ -20,7 +20,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic
 revision: str = "012_encryption_key"
-down_revision: Union[str, None] = "011_activity_logs"
+down_revision: Union[str, None] = "011_add_activity_logs_table"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
